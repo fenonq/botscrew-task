ff9110f6b83427a861928c7972b2cba89a44b54b
fenonq <fenyk.ttt@gmail.com>
2023-01-19 22:54:47 +0200

    Initial commit

---
+++ src/main/java/com/fenonq/botscrewtask/BotscrewTaskApplication.java
@@ -0,0 +1,13 @@
+package com.fenonq.botscrewtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotscrewTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotscrewTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fenonq/botscrewtask/BotscrewTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fenonq.botscrewtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotscrewTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
