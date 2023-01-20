30481b59cd768261b8cef17c164d44377734d725
fenonq <fenyk.ttt@gmail.com>
2023-01-20 19:16:06 +0200

    Added menu

--- src/test/java/com/fenonq/botscrewtask/BotscrewTaskApplicationTests.java
+++ src/test/java/com/fenonq/botscrewtask/BotscrewTaskApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BotscrewTaskApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
